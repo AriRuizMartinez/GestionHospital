b54c0ff367c41c77904976eff946a4e8153930b3
AriRuizMartinez <ariruizmartinez@gmail.com>
2024-09-25 10:13:33 +0200

    Documentacion de la clase Paciente

--- GestionHospital/Paciente.cs
+++ GestionHospital/Paciente.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace GestionHospital
 {
+    /// <summary>
+    /// Clase publica que gestiona la informacion de un paciente
+    /// </summary>
     public class Paciente : Persona
     {
         public string Enfermedad {  get; set; }
@@ -23,6 +26,9 @@ namespace GestionHospital
             return $"Paciente: Nombre: {Nombre}, Edad: {Edad}, Enfermedad: {Enfermedad}, Medico: {Medico}";
         }
 
+        /// <summary>
+        /// Metodo que avisa al medico de que deja de ser su paciente
+        /// </summary>
         public void QuitarDePacienteDeMiMedico()
         {
             Medico.EliminarPaciente(this);
