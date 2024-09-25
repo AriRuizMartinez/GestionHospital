44f691602060f211fcbdf4eac128e99a84f65df6
AriRuizMartinez <ariruizmartinez@gmail.com>
2024-09-25 10:17:42 +0200

    Documentacion de la clase PersonalHospital

--- GestionHospital/PersonalHospital.cs
+++ GestionHospital/PersonalHospital.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace GestionHospital
 {
+    /// <summary>
+    /// Clase abstracta publica que gestiona la informacion de un personal del hospital
+    /// </summary>
     public abstract class PersonalHospital : Persona
     {
         public int Sueldo {  get; set; }
